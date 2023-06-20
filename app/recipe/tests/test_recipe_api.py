c5632bc99e214757338c6ce7aad6ac7c7434154d
adan35 <adanshahzad35@gmail.com>
2023-06-21 00:51:59 +0500

    create recipe API implementation

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -94,13 +94,15 @@ class PrivateRecipeApiTests(TestCase):
     def test_create_recipe(self):
         """Test creating a recipe API"""
         payload = {
-            'tilte': 'Sample Recipe',
-            'time_minutes': 5,
-            'price': Decimal(5.3),
+            'title': 'Sample Recipe',
+            'time_minutes': 3,
+            'price': '5.9',
         }
         res = self.client.post(RECIPES_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         recipe = Recipe.objects.get(id=res.data['id'])
         for k, v in payload.items():
+            if k == 'price':
+                v = Decimal(v)
             self.assertEqual(getattr(recipe, k), v)
         self.assertEqual(recipe.user, self.user)
